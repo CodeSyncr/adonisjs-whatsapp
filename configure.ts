fac0ddbc005f321dc2c2fed8ebb9c416b49e136a
CodeSyncr <yashkumar12125@gmail.com>
2024-01-25 01:46:00 +0530

    fix: adonis v6 migration

--- configure.ts
+++ configure.ts
@@ -8,13 +8,100 @@
  */
 
 import type Configure from '@adonisjs/core/commands/configure'
+import { stubsRoot } from './stubs/main.js'
+import string from '@adonisjs/core/helpers/string'
+
+const AVAILABLE_PROVIDERS = ['lucid', 'local']
 
 export async function configure(command: Configure) {
-  // Publish config file
-  await command.publishStub('config/whatsapp.stub')
-  await command.publishStub('start/whatsapp.stub')
+  let selectedProviders: string[] | string | undefined = command.parsedFlags.providers
+
+  async function getModelName(): Promise<string> {
+    return await command.prompt.ask('Enter model name to be used for whatsapp config', {
+      validate(value) {
+        return !value || !value.trim().length ? 'Model name is required.' : true
+      },
+    })
+  }
+
+  // async function getMigrationConsent(tableName: string): Promise<boolean> {
+  //   return await command.prompt.confirm(`Create migration for the ${tableName} table?`)
+  // }
+  /**
+   * Otherwise force prompt for selection
+   */
+  if (!selectedProviders) {
+    selectedProviders = await command.prompt.multiple(
+      'Select the providers you plan to use',
+      AVAILABLE_PROVIDERS,
+      {
+        validate(value) {
+          return !value || !value.length ? 'Select a provider to configure the package' : true
+        },
+      }
+    )
+  }
+
+  /**
+   * Cast CLI string value to an array
+   */
+  let providers = (
+    typeof selectedProviders === 'string' ? [selectedProviders] : selectedProviders
+  ) as string[]
+
+  const unknownProvider = providers.find((provider) => !AVAILABLE_PROVIDERS.includes(provider))
+
+  if (unknownProvider) {
+    command.exitCode = 1
+    command.logger.error(`Invalid provider "${unknownProvider}"`)
+    return
+  }
 
   const codemods = await command.createCodemods()
+
+  if (selectedProviders === 'lucid') {
+    const modelNameInput: string = await getModelName()
+    const modelName = string
+      .create(modelNameInput.replace(/(\.ts|\.js)$/, ''))
+      .singular()
+      .pascalCase()
+    const tableName = string.create(modelName).snakeCase().plural()
+    // const modelReference = string.create(modelName).singular().camelCase()
+    // // const modelNameSpace =
+
+    // const migrationConsent = await getMigrationConsent(tableName.toString())
+    const migrationName = `${Date.now()}_${tableName}`
+
+    // const camelCaseSchemaName = string.create(tableName).camelCase().suffix('_schema')
+    if (modelNameInput) {
+      /**
+       * Publish model file
+       */
+      await codemods.makeUsingStub(stubsRoot, `make/model/whatsapp_config.stub`, {
+        name: modelNameInput,
+      })
+
+      /**
+       * Publish migration file
+       */
+      await codemods.makeUsingStub(stubsRoot, `make/migration/whatsapp_config.stub`, {
+        fileName: migrationName,
+        tableName: tableName,
+      })
+    }
+  }
+
+  /**
+   * Publish config file
+   */
+  await codemods.makeUsingStub(stubsRoot, 'config/whatsapp.stub', {
+    providers: providers,
+  })
+
+  /**
+   * Publish start file
+   */
+  await codemods.makeUsingStub(stubsRoot, 'start/whatsapp.stub', {})
 
   // Add provider to rc file
   await codemods.updateRcFile((rcFile: any) => {
--- index.ts
+++ index.ts
@@ -11,5 +11,5 @@ import './src/types/extended.js'
 
 export { configure } from './configure.js'
 export { defineConfig } from './src/define_config.js'
-export { stubsRoot } from './stubs/index.js'
+export { stubsRoot } from './stubs/main.js'
 export { default as whatsapp } from './src/whatsapp.js'
